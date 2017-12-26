5ca7f7424024cccfb83c6e42e7e1bcf877e89440
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-12-26 16:38:11 +0700

    initial commit

---
+++ src/main/java/com/github/ipan97/belajarspringbootreactive/Application.java
@@ -0,0 +1,12 @@
+package com.github.ipan97.belajarspringbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringbootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringbootReactiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/ipan97/belajarspringbootreactive/BelajarSpringbootReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.ipan97.belajarspringbootreactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelajarSpringbootReactiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
